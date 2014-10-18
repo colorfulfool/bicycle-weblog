4afb613eefff3a7120c859588d562165d39bd4d6
colorfulfool <colorfulfool@gmail.com>
2014-10-18 22:58:29 +0300

    Staticfiles -_-

--- bicycle/urls.py
+++ bicycle/urls.py
@@ -1,9 +1,14 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
+from django.conf import settings
 
 urlpatterns = patterns('',
 	(r'^grappelli/', include('grappelli.urls')), # grappelli URLS
     url(r'^admin/', include(admin.site.urls)),
+
+    url(r'^static/(?P<path>.*)$', 'django.views.static.serve', {
+        'document_root': settings.STATIC_ROOT,
+    }),
 
     url(r'^', include('blog.urls')),
 )
