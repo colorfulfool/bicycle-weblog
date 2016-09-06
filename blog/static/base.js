08b06d2ce022e63a088ad480a49f38174e5d3792
colorfulfool <t0ny.hamster@gmail.com>
2016-09-06 16:24:25 +0300

    Get rid of animations

--- blog/static/base.js
+++ blog/static/base.js
@@ -5,12 +5,12 @@ function type_in_heading(text, delay) {
 
 $(function () {
 	var img = new Image();
-	img.onload = function() {
-		$('.blog-title').html('&#x200b;')
-		type_in_heading('Bicycle for the mind', 100)
-		// setTimeout(function () {
-		// 	type_in_heading(' как ты?', 100)
-		// }, 100*11 + 700)
-	}
+	// img.onload = function() {
+	// 	$('.blog-title').html('&#x200b;')
+	// 	type_in_heading('Bicycle for the mind', 100)
+	// 	setTimeout(function () {
+	// 		type_in_heading(' как ты?', 100)
+	// 	}, 100*11 + 700)
+	// }
 	img.src = "/static/ferry.jpg"
 })
