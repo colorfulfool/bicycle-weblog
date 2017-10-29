70dc8724bb2d9164c7303011c6fa1cfc65698fa1
colorfulfool <t0ny.hamster@gmail.com>
2017-10-29 13:13:37 +0300

    localStorage back-up of form

--- blog/static/base.js
+++ blog/static/base.js
@@ -1,4 +1,4 @@
-function type_in_heading(text, delay) {
+function typeHeading(text, delay) {
 	var i = 0
 	var timer = setInterval(function () { $('.blog-title').append(text[i++]) }, delay)
 }
@@ -7,10 +7,33 @@ $(function () {
 	var img = new Image();
 	// img.onload = function() {
 	// 	$('.blog-title').html('&#x200b;')
-	// 	type_in_heading('Bicycle for the mind', 100)
+	// 	typeHeading('Bicycle for the mind', 100)
 	// 	setTimeout(function () {
-	// 		type_in_heading(' как ты?', 100)
+	// 		typeHeading(' как ты?', 100)
 	// 	}, 100*11 + 700)
 	// }
 	img.src = "/static/ferry.jpg"
 })
+
+
+$(document).ready(function () {
+	document.querySelectorAll('[data-backup][contenteditable]').forEach(function (contenteditable) {
+		contenteditable.innerHTML = fetchFromLocalStorage(contenteditable.id)
+	})
+
+	$('[data-backup][contenteditable]').change(function (event) {
+		saveToLocalStorage(event.target.id, event.target.innerHTML)
+	})	
+})
+
+function saveToLocalStorage(key, value) {
+	localStorage.setItem(key, value)
+}
+
+function fetchFromLocalStorage(key) {
+	return localStorage.getItem(key)
+}
+
+function redeemLocalStorage(key) {
+	localStorage.removeItem(key)
+}
