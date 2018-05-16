7e02d29c098fe2fe8a18201d3778048858fc6bc4
colorfulfool <t0ny.hamster@gmail.com>
2018-05-16 17:53:52 +0300

    Fix server.sh

--- server.sh
+++ server.sh
@@ -1,2 +1,4 @@
-source venv/bin/activate && python manage.py runserver --settings=bicycle.local_settings && open http://127.0.0.1:8000/
+source venv/bin/activate
+python manage.py runserver --settings=bicycle.local_settings &
+open http://127.0.0.1:8000/
 
