82f71b6fae9944a497766a7e56cf62258260a63a
colorfulfool <t0ny.hamster@gmail.com>
2018-07-09 04:06:11 +0300

    Recognize form-content.js as stretch.js

--- blog/static/behavior/stretch.js
+++ blog/static/behavior/stretch.js
@@ -1,9 +1,8 @@
 $(document).ready(function () {
-	var minHeight = 243;
-
-	document.querySelectorAll('textarea').forEach(function (textarea) {
+	document.querySelectorAll('[data-stretch-from]').forEach(function (textarea) {
 		textarea.addEventListener('input', function (event) {
 			var textarea = event.target;
+    	var minHeight = parseInt(event.target.getAttribute('data-stretch-from'));
 
 		  textarea.style.height = ""; /* Reset the height */
 		  textarea.style.height = Math.max(textarea.scrollHeight, minHeight) + "px";
